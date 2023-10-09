cfc5246a377adada2ebcbd54554fa966a167d3b6
Suporte03 <Augusto>
2023-10-09 08:06:21 -0300

    exemplo de dicionário

---
+++ dicionario.py
@@ -0,0 +1,7 @@
+dicionario = {"nome": "pedro", "idade": 25, "telefone": "32 3234-2033"}
+
+dicionario2 = {"nome": "joão", "idade": 23, "telefone": "32 3232-8855"}
+
+print(dicionario)
+
+print(dicionario2)
